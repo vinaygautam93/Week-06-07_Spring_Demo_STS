0c37d7c3d32786b32a3642c85a834cba275a8272
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 11:06:03 +0530

    [Vinay] Implemented Spring Boot App

---
+++ src/main/java/com/bridgeLabz/springconceptsapp/SpringConceptsAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.springconceptsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptsAppApplication.class, args);
+	}
+
+}
