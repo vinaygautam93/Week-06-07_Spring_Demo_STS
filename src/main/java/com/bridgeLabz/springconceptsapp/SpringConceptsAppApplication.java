e220f053565ac536f81f78fb1b9e9726ddad23df
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 11:19:26 +0530

    [Vinay] Updated SpringConceptsAppApplication.java

--- src/main/java/com/bridgeLabz/springconceptsapp/SpringConceptsAppApplication.java
+++ src/main/java/com/bridgeLabz/springconceptsapp/SpringConceptsAppApplication.java
@@ -1,13 +1,23 @@
 package com.bridgeLabz.springconceptsapp;
 
+import com.bridgeLabz.springconceptsapp.model.Employee;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringConceptsAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConceptsAppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(ApplicationContext context) {
+		return args -> {
+			Employee employee = context.getBean(Employee.class);
+			employee.showEmployeeDetails();
+		};
+	}
 }
