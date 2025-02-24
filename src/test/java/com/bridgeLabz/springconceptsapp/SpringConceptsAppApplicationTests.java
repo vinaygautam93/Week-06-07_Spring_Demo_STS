67cecada072d5dfdc8c3b5f6653231748c14b452
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-24 11:03:41 +0530

    [Vinay] Added SpringConceptsAppApplicationTests.java

---
+++ src/test/java/com/bridgeLabz/springconceptsapp/SpringConceptsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.springconceptsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConceptsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
